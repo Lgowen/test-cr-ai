c8c30bfaccebff995d6a6cfe86dd6a5d8a0a15d5
Lgowen <434666361@qq.com>
2025-03-26 20:45:08 +0800

    fix: 修复参数实效问题

--- src/args.ts
+++ src/args.ts
@@ -45,7 +45,7 @@ export const getYargs = async (): Promise<ReviewArgs> => {
       description:
         'Enables feedback to be made on a file-by-file basis. Only work when the script is running on GitHub.',
       type: 'boolean',
-      default: true,
+      default: false,
     })
     .option('model', {
       description: 'The model to use for generating the review.',
